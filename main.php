426230ca54708cedbf95bb4cc84fb536780795fc
Luka Barth <luka.costa@db1.com.br>
2025-05-27 11:07:00 -0300

    main file

--- main.php
+++ main.php
@@ -3,8 +3,12 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use App\Stack\Stack;
+use App\Queue\Queue;
 
 // Stack
+echo str_repeat("-", 30) . PHP_EOL;
+echo ("          STACK               ") . PHP_EOL;
+echo str_repeat("-", 30) . PHP_EOL;
 $addItem = new Stack(10, array('Banana', 'Apple', 'Pineapple', 'Milk', 'Cookie', 'Soap', 'Rice', 'Bean', 'Cheese', 'Meat'));
 $addItem->push('Fish');
 print_r($addItem);
@@ -18,3 +22,10 @@ echo $addItem->isEmpty();
 //__________________________________________________________________
 
 // Queue
+echo str_repeat("-", 30) . PHP_EOL;
+echo ("          QUEUE               ") . PHP_EOL;
+echo str_repeat("-", 30) . PHP_EOL;
+$queue = new Queue(array('Cookie', 'Apple', 'Pineapple', 'Milk', 'Meat'));
+$queue->peek();
+$queue->dequeue();
+print_r($queue);
